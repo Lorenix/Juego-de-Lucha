47c60102f1253558abb8c15b6aa287c39de55d7c
Loren <95588466+Lorenix@users.noreply.github.com>
2022-03-04 20:43:00 +0100

    Contenedor Personajes seleccionados + abrir fichero JS

---
+++ main.js
@@ -0,0 +1,38 @@
+/////Selección 2 jugadores
+const selectPlayer = [player1, player2, player3, player4, player5, player6];
+
+const selectPlayer = (a, b) => {
+    selectPlayer = onclick.div(a, b) 
+
+    if (player1 + player2) {
+        return pantalla2
+        } else {  
+
+    }
+)
+}
+
+
+const selectPlayer = onclick.div(player1)
+
+
+
+/////cambio de pantalla con 2 jugadores
+
+
+//Mensaje de Star fight centrado
+
+
+
+//Acciones jugadores y resto de vida
+
+ 
+
+//Gujador gananor
+
+
+ 
+//
+
+
+
