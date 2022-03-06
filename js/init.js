0eee5317e63cc5d93a2ac93ccfcdcf651cf1e2c6
Loren <95588466+Lorenix@users.noreply.github.com>
2022-03-06 22:39:16 +0100

    Comentarios en ficheros

--- js/init.js
+++
@@ -1,2 +0,0 @@
-//Abrir fichero 2 desde boton de fichero 1.
-mywindow  window.open('../html/players.html').onclick.button = new window
--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
-//Color de border en a (seleccion personaje)
-function seleccionPlayerN (seleccion, personaje) { 
-    let i = (document.getElementsByClassName("link")).onclick.style;
-        i.border = '6px solid white'
-    };
+//seleccion personaje + imprimir img de luchador seleccionado 
+
+let seleccionPlayerN = ["1","2","3","4","5","6"] ;
+let luchadorSeleccionado = "h" ;
+let abrirImg = "click" + "link()"; 
+
+link().addEventListener("click", seleccionJugador);
+
+
+function seleccionJugador() {
+    let abrirImg = document.getElementById("h").style.display = 'block'
+        for(let h = 0 ; h <= link().length ; h++);
+            return ("img" + "h") ; 
+    }
+
+let objetoDiv = document.getElementById("luchadores");
+let img = document.createElement("img");
+
+    document.getElementById("luchadores").img.innerHTML = click.value;{
+        click.Link.value = "";
+    document.getElementById("luchadores").appendChild(img);
+    }
+    
     
 
 
-//seleccion personaje + imprimir img de luchador seleccionado 
-
-var seleccionPlayerN = ["1","2","3","4","5","6"] ;
-var luchadorSeleccionado = ["1","2","3","4","5","6"] ;
-
-function seleccionPlayerN () {
-    let mostrarImg = document.getElementById("luchadores").style.display = 'block';
-    // array con array
-
-
-    let objetoDiv = document.getElementById("luchadores");
-    let img = document.createElement("img");
-
-        document.getElementById("luchadores").img.innerHTML = onclick.value;
-        clickLink.value = "";
-        document.getElementById("luchadores").appendChild(img);
-    }
-
-
+/*function seleccionPlayerN (seleccion, personaje) { 
+    let i = (document.getElementsByClassName("link")).onclick.style;
+        i.border = ''
+    };
+ */   
 
 
 
